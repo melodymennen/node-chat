326bb0b47acfc095744ab006ab0b1020b9899777
Melody <Melody@Melodys-MacBook-Pro.local>
2017-12-06 15:29:27 -0700

    step 3

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(3000, () => {
+    console.log('Server listening on port ' + 3000);
+});
